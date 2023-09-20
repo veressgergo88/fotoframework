d2828142cf37d646aaac46c29ad009193aec4cbd
veressgergo88 <veress.gergo@gmail.com>
2023-09-21 00:37:13 +0200

    svelteapp install

---
+++ svelteapp/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { loadImages } from "./lib/api";
+
+  type Image = {
+    id: number;
+    title: string;
+    url: string;
+    smallAmount: number;
+    largeAmount: number;
+  };
+
+  let isLoading = false;
+  let notificationText: string | null = null;
+  let images: Image[] = [];
+
+  let page = "home"
+
+  const search = async () => {
+    isLoading = true
+    const response = await loadImages()
+    isLoading = false
+    if (!response.success)
+      return notificationText = response.error
+    const data = response.data
+    images = data.map(img => ({...img, smallAmount: 0, largeAmount: 0}))
+  }
+
+</script>
+
+<main>
+</main>
