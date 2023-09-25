4a725d5de65b1e34809166f9efe6bd0f36ccf408
Veress Gergő <VeressG@rri.police.hu>
2023-09-25 10:49:55 +0200

    angular, react, svelte, vue foto app working

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,47 +1,67 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { loadImages } from './api';
+
+type Image = {
+    id: number;
+    title: string;
+    url: string;
+    smallAmount: number;
+    largeAmount: number;
+};
+
+let isLoading = ref(false)
+let notificationText = ref<string | null>(null)
+let images = ref<Image[]>([])
+let page = ref('home')
+let searchParam = ref("")
+
+const changePage = (newPage: string) => {
+  page.value = newPage
+}
+
+const clearSearchParam = () => {
+  searchParam.value = ""
+}
+
+const search = async () => {
+    isLoading.value = true
+    const response = await loadImages(searchParam.value)
+    isLoading.value = false
+    if (!response.success)
+      return notificationText.value = response.error
+    const data = response.data
+    images.value = data.map(img => ({...img, smallAmount: 0, largeAmount: 0}))
+    notificationText.value = null
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<div class="card">
+  <div v-if="page === 'home'">
+    <h1>Home</h1>
+    <button @click="changePage('about')">To about</button>
+    <input type="text" placeholder="Title" v-model="searchParam" />
+    <button @click="clearSearchParam">Clear</button>
+    <button @click="search">Search</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <p v-if="isLoading">Loading...</p>
+    <div v-else>
+      <div v-for="image in images" :key="image.id">
+        <p>{{ image.title }}</p>
+        <img :src="image.url" :alt="image.title" />
+      </div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <h1 v-if="notificationText">{{ notificationText }}</h1>
+  </div>
+
+  <div v-else-if="page === 'about'">
+    <h1>About</h1>
+    <button @click="changePage('home')">To Home</button>
+  </div>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
